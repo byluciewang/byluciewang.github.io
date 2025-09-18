13a7a780344fc30b0eaa388578382ba88443fdbd
Barak Binaymin <barak.binyamin555@gmail.com>
2025-09-18 19:31:56 -0400

    Added to reel, fixed up dev script

--- dev.js
+++ dev.js
@@ -8,5 +8,5 @@ app.get('/*path', (req,res)=>{
 })
 const PORT = process.env.PORT || 8001
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`Dev server is live @ http://localhost:${PORT}`)
 })
